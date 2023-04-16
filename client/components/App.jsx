a1a7fad635c49b32cda52025623a7d9768a2f75b
Samuel Lee <leesamuel423@gmail.com>
2023-04-16 05:55:24 -0400

    completed MERN-boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// DEFINE APP AS FUNCTIONAL COMPONENT AND RENDER
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+// EXPORT STATEMENT
+export default App;
+
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'client/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: {
+          loader: 'file-loader',
+        },
+      }
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+};
