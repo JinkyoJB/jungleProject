934c2baf129fcc7a11fd3e8103895720b2ff74cd
hodee <tlsghk8619@gmail.com>
2023-06-01 13:55:56 +0900

    hojun/threejs/test1

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
 
+const Box = () => {
+  const ref = useRef();
+  useFrame(state => {
+    ref.current.rotation.x += 0.02;
+    ref.current.rotation.y += 0.01;
+    
+  })
+  return(
+    <mesh ref={ref}>
+      <boxBufferGeometry/>
+      <meshDepthMaterial color='blue'/>
+    </mesh>
+  )
+}
 // DEFINE APP AS FUNCTIONAL COMPONENT AND RENDER
 const App = () => {
   return (
-    <div className="app">
-      <h1>Hello, World!</h1>
+    <div style= {{height: '100vh', width: '100wh'}}>
+      <Canvas style={{background: 'gray'}}>
+          <Box/>
+      </Canvas>
     </div>
   );
 };
